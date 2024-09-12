5692db3364cd61304b8915d8cee7ec145b554153
LePeyj <peyj73@gmail.com>
2024-09-12 18:57:11 +0800

    Update constants.dart

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 const double kOverlayRatio = 0.65;
-const double kLabelBoxWidthRatio = 0.5;
-const double kLabelBoxWidthRatioLarge = 0.8;
+const double kLabelBoxWidthRatio = 1;
+const double kLabelBoxWidthRatioLarge = 1;
 const double kLabelBoxHeightRatio = 0.45;
 const double kArrowHeight = 16.0;
 const double kSpace = 4.0;
